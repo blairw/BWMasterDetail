0c5891be0e739b80d56c283a07291776991c8e0c
Blair <hello@blair.wang>
2021-12-22 17:07:39 +1100

    [feat] Initial commit

---
+++ BWMasterDetail.js
@@ -0,0 +1,79 @@
+var MOBILE_THRESHOLD = 600;
+
+var pageInitialised = false;
+var selectedItem = null;
+var pageMode = "DESKTOP";
+
+function bodyDidLoad() {
+	attachHandlers();
+	switchToDetailBlock("Thing1");
+
+	if (window.innerWidth <= MOBILE_THRESHOLD) {
+		pageMode = "MOBILE";
+
+		// User lands on the menu rather than the default-selected item
+		$("#BWMasterDetail_Detail").hide();
+	}
+
+	pageInitialised = true;
+}
+
+function switchToDetailBlock(thisDetailBlock) {
+	// Actual moving to the block
+	$(".DetailBlock").hide();
+	$("#" + thisDetailBlock).show();
+
+	// Highlighting
+	$(".BWMasterDetail_MasterItem").removeClass("is-active");
+	$("#" + "BWMasterDetail_MasterItemFor_" + thisDetailBlock).addClass("is-active");
+
+	// Update title
+	$("#CurrentItemHeading").html(thisDetailBlock)
+
+	// Record the change
+	selectedItem = thisDetailBlock;
+}
+
+function respondToWindowResize() {
+	if (window.innerWidth <= MOBILE_THRESHOLD && pageMode == "DESKTOP") {
+		// Moving from Desktop to Mobile
+		pageMode = "MOBILE";
+
+		// No further action required
+	}
+
+	if (window.innerWidth > MOBILE_THRESHOLD && pageMode == "MOBILE") {
+		// Moving from Mobile to Desktop
+		pageMode = "DESKTOP";
+
+		$("#BWMasterDetail_Master").show();
+		$("#BWMasterDetail_Detail").show();
+	}
+}
+
+function attachHandlers() {
+	$(".BWMasterDetail_MasterItem").on("click", function () {
+		var thisId = $(this).attr('id');
+		var thisDetailBlock = thisId.replace("BWMasterDetail_MasterItemFor_", "");
+		switchToDetailBlock(thisDetailBlock);
+
+		if (pageInitialised && window.innerWidth <= MOBILE_THRESHOLD) {
+			$("#BWMasterDetail_Detail").show("slide", { direction: "right" }, 500);
+			$("#BWMasterDetail_Master").hide("slide", { direction: "left" }, 700);
+		}
+	});
+
+	$("#BackButton").on("click", function () {
+		if (window.innerWidth <= MOBILE_THRESHOLD) {
+			$("#BWMasterDetail_Detail").hide("slide", { direction: "right" }, 700);
+			$("#BWMasterDetail_Master").show("slide", { direction: "left" }, 500);
+		}
+	})
+
+	// Adapted from https://stackoverflow.com/questions/599288/cross-browser-window-resize-event-javascript-jquery
+	let resizeTimer = 0;
+	$(window).on("resize", function () {
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(respondToWindowResize(), 100);
+	});
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # BWMasterDetail
+
+![Screenshot](docs/screenshot.png)
+
+_BWMasterDetail_ is an implementation of a responsive master-detail view using jQuery and jQuery UI.
+
+- When the screen or window is **wider than 600 pixels (i.e., tablet/desktop view)**, you get the full view with master and detail both concurrently visible.
+
+- When the screen or window is **narrower than 600 pixels (i.e., mobile view)**, you only see the master menu at first, then you can click into one of the detail items.
+
+This is the user experience of typical iOS and Android apps including:
+
+- Email apps (master: list of emails; detail: content of the selected email)
+- Messaging apps (master: list of chats; detail: chat log of the selected chat)
+- Settings apps (master: category i.e., Wifi/Bluetooth/etc.; detail: settings for selected category)
+
+This is much more appropriate than the typical Bootstrap navigation-bar-turning-into-menu user experience in situations where there are many menu items, or the menu items have long(ish) names, so you can avoid the situation with an awfully cramped navigation bar at the top of the screen.
+
+Although others have implemented similar things in [Angular](https://blewpri.medium.com/angular-8-responsive-master-detail-3b4d591b3294), [React](https://blewpri.medium.com/react-responsive-master-detail-10b60b739879) and [Flutter](https://bettercoding.dev/flutter/responsive-master-detail/), I wanted something that I could use without being locked into one of those heavier frameworks. To be fair, jQuery is still a framework, but it's a significantly more flexible one. Licensed under MIT, please feel free to use on your projects :)
